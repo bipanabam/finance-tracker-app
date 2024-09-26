f53f60ab99fb0c8c09126ea135306d52c9e1f947
Bipana Bam <bipanabam147@gmail.com>
2024-09-26 18:14:13 +0545

    htmx form submission

--- tracker/models.py
+++ tracker/models.py
@@ -22,9 +22,13 @@ class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     type = models.CharField(max_length=7, choices=TRANSACTION_TYPE_CHOICES)
+    description = models.TextField(null=True, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
 
     def __str__(self):
         return f"{self.type} of {self.amount} on {self.date} by {self.user}"
+    
+    class Meta:
+        ordering = ["-date"]
     
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from .models import Transaction
+from .filters import TransactionFilter
 
 # Create your views here.
 def index(request):
     return render(request, 'tracker/index.html')
+
+def transaction_list(request):
+    transaction_filter = TransactionFilter(
+        request.GET,
+        queryset=Transaction.objects.filter(user=request.user).select_related("category")
+    )
+    # print(transactions)
+    context = {'filter': transaction_filter}
+    if request.htmx:
+        return render(request, 'tracker/partials/transaction-container.html', context)
+    return render(request, 'tracker/transaction-list.html', context)
